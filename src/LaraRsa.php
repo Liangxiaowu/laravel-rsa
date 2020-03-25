39c613a7e46fa8e4bfcdac473f1b36f3d94d8cb8
xiaowu <lxiaowud@163.com>
2020-03-25 15:31:35 +0800

    更新

--- src/LaraRsa.php
+++ src/LaraRsa.php
@@ -20,27 +20,6 @@ class LaraRsa
 
     private static function params($name, $arguments){
         $RsaKey = RsaKey::getIns();
-        $count = count($arguments);
-        switch ($count){
-            case 1:
-                $result = $RsaKey->$name($arguments[0]);
-                break;
-            case 2:
-                $result = $RsaKey->$name($arguments[0],$arguments[1]);
-                break;
-            case 3:
-                $result = $RsaKey->$name($arguments[0],$arguments[1],$arguments[2]);
-                break;
-            case 4:
-                $result = $RsaKey->$name($arguments[0],$arguments[1],$arguments[2],$arguments[3]);
-                break;
-            case 5:
-                $result = $RsaKey->$name($arguments[0],$arguments[1],$arguments[2],$arguments[3],$arguments[4]);
-                break;
-            default:
-                $result = $RsaKey->$name();
-                break;
-        }
-        return $result;
+        return $RsaKey ->$name(...$arguments);
     }
 }
